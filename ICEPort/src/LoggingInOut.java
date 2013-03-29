17fd74193952cad453d7173d13a689d5c6824b93
Mederic Blandin <mederic.blandin@telecom-bretagne.eu>
2013-03-29 16:58:37 +0700

    I just wrote some classes to start!

---
+++ ICEPort/src/LoggingInOut.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+
+public class LoggingInOut {
+	
+	private ArrayList<String> listStudent = new ArrayList<String>();
+	private String username;
+	private String password;
+	
+	public LoggingInOut(ArrayList<String> listStudent){
+		this.listStudent = listStudent;
+				
+	}
+
+	public void loggingIn(){
+		
+	}
+}
