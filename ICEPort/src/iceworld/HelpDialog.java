d7688669ac0f024d3fb49e8ce0e5f355ffb19440
vicky2872 <vicky_2872@hotmail.com>
2013-04-16 21:54:26 +0700

    help dialog w/ link

--- ICEPort/src/iceworld/HelpDialog.java
+++ ICEPort/src/iceworld/HelpDialog.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 
 import javax.swing.JEditorPane;
 import javax.swing.JTabbedPane;
+import javax.swing.ToolTipManager;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 
 public class HelpDialog extends JTabbedPane{
 
@@ -14,23 +17,52 @@ public class HelpDialog extends JTabbedPane{
 	}
 
 	private void addTabs(){
-		this.add("Basic", makeHtmlView());
-		this.add("Advanced", makeHtmlView());
+		this.add("Basic", makeHtmlView("basic"));
+		this.add("Advanced", makeHtmlView("advanced"));
 	}
 	
-	private JEditorPane makeHtmlView(){
-		JEditorPane jep = new JEditorPane();
+	private JEditorPane makeHtmlView(String mode){
+		final JEditorPane jep = new JEditorPane();
 		jep.setEditable(false);
 		
+		
+		ToolTipManager.sharedInstance().registerComponent(jep);
+		 
+        HyperlinkListener l = new HyperlinkListener() {
+            @Override
+            public void hyperlinkUpdate(HyperlinkEvent e) {
+                if (HyperlinkEvent.EventType.ACTIVATED == e.getEventType()) {
+                    try {
+                        jep.setPage(e.getURL());
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+
+            }
+
+        };
+        jep.addHyperlinkListener(l);
+		
 		try{
-			jep.setPage("http://cerntainly.com/demo/HW10/ICEbook.html");
+			if(mode.equals("basic")){
+				jep.setPage("https://dl.dropboxusercontent.com/u/97755864/basic.html");
+ 
+			}else if(mode.equals("advanced")){
+				jep.setPage("https://dl.dropboxusercontent.com/u/97755864/advance/advance.html"); // this should work
+			}
 		} catch(IOException e){
 			jep.setContentType("text/html");
 			jep.setText("<html>Could not load</html>");
 		}
+		
 		jep.setPreferredSize(new Dimension(500,500));
 		return jep;
 	}
+
+	// okay.. please put this on pastebin for me thank you
+	// and don't remove the html files from your dropbox yet na I'll have to copy
+	// and put on my cerntainly.com host
 	
 	
 }
